dd1347a48ad881b63da2efed710cdf9d06f5bab0
magladko <kornel.marek@gmail.com>
2022-03-20 21:31:06 +0100

    Loaded files.

--- mpp1/kNN.py
+++ mpp1/kNN.py
@@ -1,4 +1,10 @@
+import pandas as pd
+
+
+def main():
+    training_data_file = pd.read_csv('mpp_files/iristrain.csv')
+    print(training_data_file.head())
 
 
 if __name__ == '__main__':
-    pass
+    main()
