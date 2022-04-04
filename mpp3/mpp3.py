e4b3a4b17a2d4cfdc0c88d6eccda5c221175e075
magladko <kornel.marek@gmail.com>
2022-04-04 15:29:33 +0200

    NAI mpp3

--- mpp3/mpp3.py
+++ mpp3/mpp3.py
@@ -6,16 +6,9 @@ import os
 
 
 class Perceptron:
-    def __init__(self, language, theta=0, alpha=0.1, beta=0.1 ):
-        # self.X = np.zeros(ord("z")-ord("a")+1) # without theta
-        # self.X = np.zeros(ord("z")-ord("a")+2) # with theta
-        # self.X[-1] = -1
+    def __init__(self, language, theta=0, alpha=0.1, beta=0.1):
         self.lang = language
-
-        self.W = np.random.rand(ord("z")-ord("a")+1) # without theta
-        # self.W = np.random.rand(ord("z")-ord("a")+2) # with theta
-        # self.W[-1] = theta
-
+        self.W = np.random.rand(ord("z")-ord("a")+1)
         self.theta = theta
         self.alpha = alpha
         self.beta = beta
@@ -26,34 +19,69 @@ class Perceptron:
     def calc_discrete_output(self, X):
         return 1 if self.calc_linear_output(X) >= self.theta else 0
 
-    def train(self, langs_dir, epochs):
-        guess = 0
-        answer = 1 if langs_dir.split("/")[-1] == self.lang else 0
+    def test_all(self, langs_dir):
+        true_positives = 0
+        true_negatives = 0
+        false_positives = 0
+        false_negatives = 0
 
+        X = np.zeros(ord("z")-ord("a")+1)
+
+        for root, dirs, files in os.walk(langs_dir):
+            answer = 1 if root.split("\\")[-1] == self.lang else 0
+            for file in files:
+                delete_non_ascii_characters(os.path.join(root, file))
+                X = count_distinct_letters(os.path.join(root, file))
+                guess = self.calc_discrete_output(X)
+                if guess == answer:
+                    if guess == 1:
+                        true_positives += 1
+                    else:
+                        true_negatives += 1
+                else:
+                    if guess == 1:
+                        false_positives += 1
+                    else:
+                        false_negatives += 1
+
+        return {"true_positives": true_positives, "true_negatives": true_negatives, "false_positives": false_positives,
+                "false_negatives": false_negatives}
+
+    def guess_linear(self, lang_doc):
+        delete_non_ascii_characters(lang_doc)
+        X = count_distinct_letters(lang_doc)
+        return self.calc_linear_output(X)
+
+    def train(self, langs_dir, epochs):
         X = np.zeros(ord("z")-ord("a")+1)
 
         for i in range(epochs):
             for root, dirs, files in os.walk(langs_dir):
+                answer = 1 if root.split("\\")[-1] == self.lang else 0
                 for file in files:
                     # print(os.path.join(root, file))
                     delete_non_ascii_characters(os.path.join(root, file))
                     X = count_distinct_letters(os.path.join(root, file))
                     guess = self.calc_discrete_output(X)
 
-            self.W += (guess - answer) * self.alpha * X
-            # normalize weights
-            self.W /= np.linalg.norm(self.W)
+                    # if self.lang == "hiszpanski":
+                    #     print(root.split("\\")[-1] + "\t\t" + str(guess), answer-guess, self.theta)
 
-            self.theta -= (guess - answer) * self.beta
-        # print Perceptron properties
+                    self.W += (answer - guess) * self.alpha * X
+                    self.theta -= (answer - guess) * self.beta
+            # normalize weights
+            self.W /= np.linalg.norm(self.W, ord=2)
+
+    def print_properties(self):
+        print(self.lang)
         print("W:", self.W)
         print("theta:", self.theta)
+        print()
 
 
 # read all files in the directory recursively
 def read_files(directory):
     res = []
-    # langs = os.listdir(directory)
     for root, dirs, files in os.walk(directory):
         for file in files:
             res.append(os.path.join(root, file))
@@ -63,12 +91,10 @@ def read_files(directory):
 # create method deleting all non-ascii characters from file and save it
 def delete_non_ascii_characters(file):
     parsed_path = "\\".join(file.split('\\')[:-1])+'\\'+'parsed_'+file.split("\\")[-1]
-
-    with open(file, 'r', encoding='utf8') as infile, open(parsed_path, 'w',encoding='ascii') as outfile:
+    with open(file, 'r', encoding='utf8') as infile, open(parsed_path, 'w', encoding='ascii') as outfile:
         for line in infile:
-            # print(line)
             try:
-                outfile.write(''.join(i.lower() for i in line if ord('z') >= ord(i) >= ord('a') or ord('Z') >= ord(i) >= ord('A')))
+                outfile.write(''.join(i.lower() for i in line if ord('z') >= ord(i) >= ord('a')))
             except UnicodeDecodeError:
                 pass
     os.remove(file)
@@ -82,15 +108,84 @@ def count_distinct_letters(file):
         for line in f:
             for char in line:
                 res[ord(char.lower())-ord("a")] += 1
-                # res[(ord(char)-ord('a')) % len(res)] += 1
     return res
 
 
-# print(read_files("langs"))
+def plot_input_for_lang(lang, langs_dir):
+    X = np.zeros(ord("z")-ord("a")+1)
+    for root, dirs, files in os.walk(langs_dir):
+        if root.split("\\")[-1] == lang:
+            for file in files:
+                delete_non_ascii_characters(os.path.join(root, file))
+                X += count_distinct_letters(os.path.join(root, file))
 
-p = Perceptron("czeski")
-p.train("langs", 50)
+    X /= np.linalg.norm(X, ord=2)
+    plt.plot(X)
 
-# np.norm(p.W)
-# numpy second norm of vector:
-print(np.linalg.norm(p.W, ord=2))
+
+def guess_language(doc_path, perceptrons):
+    p_guess = dict()
+    for p in perceptrons:
+        p_guess[p.lang] = p.guess_linear(doc_path)
+    return p_guess.get(max(p_guess, key=p_guess.get))
+
+
+plot_input_for_lang("polski", "langs")
+plot_input_for_lang("niemiecki", "langs")
+plot_input_for_lang("hiszpanski", "langs")
+plt.legend(["polski", "niemiecki", "hiszpanski"])
+plt.show()
+
+p_pl = Perceptron("polski")
+p_de = Perceptron("niemiecki")
+p_es = Perceptron("hiszpanski", alpha=0.001, beta=0.001)
+
+p_pl.train("langs", 1)
+p_de.train("langs", 1)
+p_es.train("langs", 10)
+
+p_pl.print_properties()
+p_de.print_properties()
+p_es.print_properties()
+
+print("pl:", p_pl.test_all("langs"))
+print("de:", p_de.test_all("langs"))
+print("es:", p_es.test_all("langs"))
+
+# plot weights for each perceptron
+plt.plot(p_pl.W, label="pl")
+plt.plot(p_de.W, label="de")
+plt.plot(p_es.W, label="es")
+plt.legend()
+plt.show()
+
+# testowy
+# plot_input_for_lang("jezykA", "testowy")
+# plot_input_for_lang("jezykB", "testowy")
+# plot_input_for_lang("jezykC", "testowy")
+#
+# plt.legend(["jezykA", "jezykB", "jezykC"])
+# plt.show()
+#
+# p_a = Perceptron("jezykA")
+# p_b = Perceptron("jezykB")
+# p_c = Perceptron("jezykC")
+#
+# p_a.train("testowy", 200)
+# p_b.train("testowy", 200)
+# p_c.train("testowy", 200)
+#
+# p_a.print_properties()
+# p_b.print_properties()
+# p_c.print_properties()
+#
+# print(p_a.test_all("testowy"))
+# print(p_b.test_all("testowy"))
+# print(p_c.test_all("testowy"))
+#
+# # plot weights for each perceptron
+# plt.plot(p_a.W, label="a")
+# plt.plot(p_b.W, label="b")
+# plt.plot(p_c.W, label="c")
+# plt.legend()
+# plt.show()
